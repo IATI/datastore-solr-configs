f4d933678d7b9ac04288e58a4bf2d3ebacba27a3
nosvalds <niko@devinit.org>
2021-11-22 09:54:15 +0000

    merge configset changes, use collection specific templates

--- schema-converter/convertSchema.sh
+++ schema-converter/convertSchema.sh
@@ -16,9 +16,9 @@ fi
 for COLPATH in ../../configsets/* ; do
     COLLECTION=$(basename "$COLPATH")
     echo "converting schema for: "$COLLECTION
-    SOLRCONFIG_TEMPLATE="../solrconfig-template.xml"
+    SOLRCONFIG_TEMPLATE="../templates/${COLLECTION}/solrconfig-template.xml"
     SOLRCONFIG_DEST="../../configsets/${COLLECTION}/conf/solrconfig.xml"
-    SOLRSCHEMA_TEMPLATE="../managed-schema-template.xml"
+    SOLRSCHEMA_TEMPLATE="../templates/${COLLECTION}/managed-schema-template.xml"
     SOLRSCHEMA_DEST="../../configsets/${COLLECTION}/conf/managed-schema"
     python gen_solr.py $COLLECTION $SOLRCONFIG_TEMPLATE $SOLRCONFIG_DEST $SOLRSCHEMA_TEMPLATE $SOLRSCHEMA_DEST
 done
